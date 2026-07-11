b5f198b2047a86b93e3bbdb8a0ad34b05396fc91
DannielYu <yubolinqwer@163.com>
2026-07-11 22:02:09 +0800

    Initial Commit

---
+++ Velum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Velum
+//
+//  Created by Danniel Yu on 7/11/R8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Velum/VelumApp.swift
@@ -0,0 +1,17 @@
+//
+//  VelumApp.swift
+//  Velum
+//
+//  Created by Danniel Yu on 7/11/R8.
+//
+
+import SwiftUI
+
+@main
+struct VelumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
